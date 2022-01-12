43ce77071e2520fdb46669ad0b27e262f480d094
dyeggo bezerra <dyeggo0102@live.com>
2022-01-12 19:57:14 -0300

    Pagination layout

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,19 @@
+import './styler.css';
+import {ReactComponent as Seta} from 'assets/img/seta.svg';
+function Pagination() {
+    return (
+        <div className="dsmovie-pagination-container">
+            <div className="dsmovie-pagination-box">
+                <button className="dsmovie-pagination-button" disabled={true} >
+                    <Seta />
+                </button>
+                <p>{`${1} de ${3}`}</p>
+                <button className="dsmovie-pagination-button" disabled={false} >
+                    <Seta className="dsmovie-flip-horizontal" />
+                </button>
+            </div>
+        </div>
+
+    );
+}
+export default Pagination;
